8804cd85f8026ead354a8ed9fe495cfc63d3a8e1
souravmishra7456 <mishra.sourav79@gmail.com>
2025-07-09 00:10:50 +0530

    favicon updated

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -95,10 +95,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="vlbin4aTrQE8zViQM6FhohFDbWpx3KUAmM5PsFZBwkw" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#7c3aed" />
         <meta name="msapplication-TileColor" content="#7c3aed" />
